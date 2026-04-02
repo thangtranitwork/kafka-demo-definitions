5e211ceda89ee85ce9eeebe62f7e2d499d015d6a
Thắng Trần <thangtran.it.work@gmail.com>
2026-04-02 17:52:21 +0700

    use uint instead of int for order created event

--- events/order-created-event.go
+++ events/order-created-event.go
@@ -1,8 +1,8 @@
 package events
 
 type OrderCreatedEvent struct {
-	OrderID   int     `json:"order_id"`
-	UserID    int     `json:"user_id"`
+	OrderID   uint    `json:"order_id"`
+	UserID    uint    `json:"user_id"`
 	Amount    float64 `json:"amount"`
 	CreatedAt int64   `json:"created_at"`
 }

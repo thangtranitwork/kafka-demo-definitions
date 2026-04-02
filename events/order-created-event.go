a2ed6f9f229c8ab6783440589437e5a1a572c103
Thắng Trần <thangtran.it.work@gmail.com>
2026-04-02 17:50:14 +0700

    use int instead of string for order created event

--- events/order-created-event.go
+++ events/order-created-event.go
@@ -1,8 +1,8 @@
 package events
 
 type OrderCreatedEvent struct {
-	OrderID   string  `json:"order_id"`
-	UserID    string  `json:"user_id"`
+	OrderID   int     `json:"order_id"`
+	UserID    int     `json:"user_id"`
 	Amount    float64 `json:"amount"`
 	CreatedAt int64   `json:"created_at"`
 }
